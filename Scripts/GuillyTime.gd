58f0c0efbe8b04d39080a1dbaf6bc7f2ae3cc78f
d.grison <david.grison@groupe-creative.fr>
2024-01-27 18:02:13 +0100

    deplacement script

--- Scripts/GuillyTime.gd
+++ Scripts/GuillyTime.gd
@@ -1,17 +1,22 @@
 extends MarginContainer
 
 var mrGuilly
+var finishLabel 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	mrGuilly=$MonsieurGuilly
+	finishLabel=$LabelFinishHim
+	finishLabel.text = "Finish Him"
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	tickle()
 	pass
 	
 func tickle():
-	mrGuilly.
+	if(mrGuilly.isKo):
+		finishLabel.text = "You WIN !"
 	pass
