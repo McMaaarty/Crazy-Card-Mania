0c86b3549648ec596d8629a352f2778c2eb88c98
d.grison <david.grison@groupe-creative.fr>
2024-01-27 17:40:57 +0100

    Creation MrGuilly avec determination de son statut KO et de son total de PdV

---
+++ Scripts/GuillyTime.gd
@@ -0,0 +1,17 @@
+extends MarginContainer
+
+var mrGuilly
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	mrGuilly=$MonsieurGuilly
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func tickle():
+	mrGuilly.
+	pass
