76e207faaf20f1736d4898605781de5fbf977b26
Iron-Wolf <Iron-Wolf@users.noreply.github.com>
2024-01-27 18:15:23 +0100

    init scene inventory

---
+++ Scripts/Inventory.gd
@@ -0,0 +1,40 @@
+extends MarginContainer
+
+var inventoryList:FlowContainer
+var playerList:FlowContainer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var cardInventory = preload("res://Scenes/Objects/CardInventory.tscn")
+	inventoryList = get_node("BoxContainer/InventoryCardList")
+	playerList = get_node("BoxContainer/PlayerCardList")
+	
+	for id in StaticData.inventoryCardIds:
+		var cardInventoryInst = cardInventory.instantiate()
+		cardInventoryInst.pressed.connect(moveToPlayer.bind(cardInventoryInst))
+		inventoryList.add_child(cardInventoryInst)
+	
+	for id in StaticData.playerCardIds:
+		var cardInventoryInst = cardInventory.instantiate()
+		cardInventoryInst.pressed.connect(moveToInventory.bind(cardInventoryInst))
+		playerList.add_child(cardInventoryInst)
+	
+
+func moveToPlayer(cardInventoryInst):
+	cardInventoryInst.pressed.disconnect(moveToPlayer)
+	inventoryList.remove_child(cardInventoryInst)
+	playerList.add_child(cardInventoryInst)
+	cardInventoryInst.pressed.connect(moveToInventory.bind(cardInventoryInst))
+	
+
+func moveToInventory(cardInventoryInst):
+	cardInventoryInst.pressed.disconnect(moveToInventory)
+	playerList.remove_child(cardInventoryInst)
+	inventoryList.add_child(cardInventoryInst)
+	cardInventoryInst.pressed.connect(moveToPlayer.bind(cardInventoryInst))
+	
+	
+
+
+
+
